b52eff50d559d4717227c82aad402bcefe89d5de
developingbear <developingbear@gmail.com>
2021-01-31 21:06:54 +0900

    first commit

---
+++ src/test/java/bside/writing/WritingApplicationTests.java
@@ -0,0 +1,13 @@
+package bside.writing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WritingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
