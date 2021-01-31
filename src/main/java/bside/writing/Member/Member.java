f8ac80f173b02f65f1d1f15dab5962dd98f5d8ba
developingbear <developingbear@gmail.com>
2021-01-31 21:34:15 +0900

    add : Member member&methods

--- src/main/java/bside/writing/Member/Member.java
+++ src/main/java/bside/writing/Member/Member.java
@@ -1,6 +1,56 @@
 package bside.writing.Member;
 
-public class Member {
-    private final Long userNumber = 10L;
 
+import java.util.List;
+
+public class Member {
+    private final Long userNumber;
+    private String nickName;
+    private String EmailAddress;
+    private List<Long> ArticleNumberList;
+
+    public Member(Long userNumber, String nickName, String emailAddress, List<Long> articleNumberList) {
+        this.userNumber = userNumber;
+        this.nickName = nickName;
+        EmailAddress = emailAddress;
+        ArticleNumberList = articleNumberList;
+    }
+
+    public Long getUserNumber() {
+        return userNumber;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getEmailAddress() {
+        return EmailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        EmailAddress = emailAddress;
+    }
+
+    public List<Long> getArticleNumberList() {
+        return ArticleNumberList;
+    }
+
+    public void setArticleNumberList(List<Long> articleNumberList) {
+        ArticleNumberList = articleNumberList;
+    }
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "userNumber=" + userNumber +
+                ", nickName='" + nickName + '\'' +
+                ", EmailAddress='" + EmailAddress + '\'' +
+                ", ArticleNumberList=" + ArticleNumberList +
+                '}';
+    }
 }
