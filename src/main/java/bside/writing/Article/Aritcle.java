0780669cba5d1255d893e659a03a1d29d3f1a257
kumakuma34 <qgqg264@gmail.com>
2021-02-01 23:19:48 +0900

    feat: initial Article Class
    first version of Article Class
    
    Signed-off-by: kumakuma34 <qgqg264@gmail.com>

--- src/main/java/bside/writing/Article/Aritcle.java
+++ src/main/java/bside/writing/Article/Aritcle.java
@@ -1,6 +1,82 @@
 package bside.writing.Article;
 
-public class Aritcle {
-    private String name;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+public class Aritcle {
+    private Long userNumber;
+    private Long articleNumber;
+    private String title;
+    private String content;
+    private Date registerDate;
+    private Date modifyDate;
+
+    public Aritcle(Long userNumber, Long articleNumber, String title, String content, Date registerDate, Date modifyDate) {
+        this.userNumber = userNumber;
+        this.articleNumber = articleNumber;
+        this.title = title;
+        this.content = content;
+        this.registerDate = registerDate;
+        this.modifyDate = modifyDate;
+    }
+
+    public Long getUserNumber() {
+        return userNumber;
+    }
+
+    public void setUserNumber(Long userNumber) {
+        this.userNumber = userNumber;
+    }
+
+    public Long getArticleNumber() {
+        return articleNumber;
+    }
+
+    public void setArticleNumber(Long articleNumber) {
+        this.articleNumber = articleNumber;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getRegisterDate() {
+        return registerDate;
+    }
+
+    public void setRegisterDate(Date registerDate) {
+        this.registerDate = registerDate;
+    }
+
+    public Date getModifyDate() {
+        return modifyDate;
+    }
+
+    public void setModifyDate(Date modifyDate) {
+        this.modifyDate = modifyDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Aritcle{" +
+                "userNumber=" + userNumber +
+                ", articleNumber=" + articleNumber +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                ", registerDate=" + registerDate +
+                ", modifyDate=" + modifyDate +
+                '}';
+    }
 }
