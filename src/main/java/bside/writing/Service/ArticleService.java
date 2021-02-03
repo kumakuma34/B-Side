6db01359a77214ba04cf8150c6ec4d5dd2ef4e47
kumakuma34 <qgqg264@gmail.com>
2021-02-03 20:16:01 +0900

    fix: Modified ArticleService.java
    modified ArticleService.java
    
    Signed-off-by: kumakuma34 <qgqg264@gmail.com>

--- src/main/java/bside/writing/Service/ArticleService.java
+++ src/main/java/bside/writing/Service/ArticleService.java
@@ -1,2 +1,30 @@
-package bside.writing.Service;public class ArticleService {
+package bside.writing.Service;
+
+import bside.writing.Article.*;
+import bside.writing.Repository.ArticleRepository;
+import bside.writing.Repository.MemoryArticleRepository;
+
+import java.awt.geom.AffineTransform;
+import java.util.List;
+import java.util.Optional;
+
+public class ArticleService {
+    private final ArticleRepository articleRepository = new MemoryArticleRepository();
+
+    public Article saveArticle(Article saveArticle){
+        return articleRepository.save(saveArticle);
+    }
+
+    public Optional<Article> deleteArticle(Long deleteArticleNumber){
+        return articleRepository.delete(deleteArticleNumber);
+    }
+
+    public List<Article> searchByTitle(String searchTitle){
+        return articleRepository.searchByTitle(searchTitle);
+    }
+
+    public List<Article> loadArticle(Long memberId){
+        return articleRepository.searchByRegisterId(memberId);
+    }
+
 }
