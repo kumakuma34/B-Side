9900871dd9fd2f6617f8c66e4be1e807d6882d49
kumakuma34 <qgqg264@gmail.com>
2021-02-13 01:26:37 +0900

    feat: GET Method for /members
    created GET Method for /members URI
    Signed-off-by: kumakuma34 <qgqg264@gmail.com>

--- src/main/java/bside/writing/controller/MemberController.java
+++ src/main/java/bside/writing/controller/MemberController.java
@@ -1,5 +1,7 @@
 package bside.writing.controller;
 
+import bside.writing.Member.Member;
+import bside.writing.Repository.MemberRepository;
 import bside.writing.Service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -8,14 +10,19 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class MemberController {
-    private final MemberService memberService;
-    @Autowired
-    public MemberController(MemberService memberService) {
-        this.memberService = memberService;
-    }
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
 
+    @RequestMapping(value = "/members", method = RequestMethod.GET)
+    @ResponseStatus(value = HttpStatus.OK)
+    public List<Member> getAllMembers(){
+        List<Member> allMembers = memberRepository.findAll();
+        return allMembers;
+    }
 
     /* POSTMAN API 호출 테스트
     @RequestMapping(value = "/api", method = RequestMethod.GET)
