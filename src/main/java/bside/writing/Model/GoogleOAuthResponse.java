c9f812f2f3a15cb884d1b395811e3ccd76fda3db
developingbear <developingbear@gmail.com>
2021-02-16 15:11:55 +0900

    feat: Google OAuth login test
    구글 OAuth로그인 테스트코드 작성
    Model에 토큰 관련 class 생성
    Contoller dir에 GoogleOAuthLoginController 생성

---
+++ src/main/java/bside/writing/Model/GoogleOAuthResponse.java
@@ -0,0 +1,14 @@
+package bside.writing.Model;
+
+import lombok.Data;
+
+@Data
+public class GoogleOAuthResponse {
+    private String accessToken;
+    private String expiresIn;
+    private String refreshToken;
+    private String scope;
+    private String tokenType;
+    private String idToken;
+
+}
