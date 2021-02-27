5eede7124fa91b49caa779e239c1e6957e1fa5c2
developingbear <developingbear@gmail.com>
2021-02-27 19:53:05 +0900

    refactor: Delete access token test code
    access token 샘플 테스트 코드 삭제
    - 관련 class 삭제

--- src/main/java/bside/writing/Service/Security/SecurityService.java
+++
@@ -1,9 +0,0 @@
-package bside.writing.Service.Security;
-import org.springframework.stereotype.Service;
-
-@Service
-public interface SecurityService {
-    String createToken(String subject, long ttlMillis);
-    String getEmail(String token);
-    String getName(String token);
-}
