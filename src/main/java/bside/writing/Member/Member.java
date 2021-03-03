5d45f37239a52965ba1af3321cdb699de66d2657
developingbear <developingbear@gmail.com>
2021-03-03 20:54:08 +0900

    feat: Apply lombok
    맴버 클래스에 롬복적용

--- src/main/java/bside/writing/Member/Member.java
+++ src/main/java/bside/writing/Member/Member.java
@@ -1,10 +1,8 @@
 package bside.writing.Member;
 
-
-import com.fasterxml.jackson.annotation.JsonAnySetter;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.annotation.Id;
-import com.fasterxml.jackson.annotation.JsonCreator;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 import org.hibernate.annotations.CreationTimestamp;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -13,96 +11,37 @@ import javax.persistence.GenerationType;
 import javax.persistence.*;
 import java.beans.ConstructorProperties;
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Entity
 @Table(name = "member")
+@Getter
+@Setter
+@ToString
 public class Member {
     @javax.persistence.Id
     @GeneratedValue(strategy =  GenerationType.IDENTITY)
     private Long member_id;
 
     @Column(length = 40)
-    private String email_address;
+    private String email;
 
     @Column(length = 100)
-    private String  nick_name;
+    private String  name;
 
-    @CreationTimestamp //Audting 처리 예정
+    @CreationTimestamp
     private LocalDateTime reg_dt;
 
     @Column(length = 255)
-    private String user_auth;
+    private String userRole;
 
     @Column(length = 255)
     private String pictureURL;
 
-    @ConstructorProperties({"email_address", "nick_name", "userRole", "pictureURL"})
-    public Member(String email_address , String nick_name , String userRole, String pictureURL) {
-        this.email_address = email_address;
-        this.nick_name = nick_name;
-        //this.reg_dt = reg_dt;
-        this.user_auth = userRole;
+    @ConstructorProperties({"email", "name", "userRole", "pictureURL"})
+    public Member(String email , String name , String userRole, String pictureURL) {
+        this.email = email;
+        this.name = name;
+        this.userRole = userRole;
         this.pictureURL = pictureURL;
-    }
-
-    public Member(){};//default Constructor
-
-    public Long getMember_id() {
-        return member_id;
-    }
-
-    public void setMember_id(Long member_id) {
-        this.member_id = member_id;
-    }
-
-    public String getEmail_address() {
-        return email_address;
-    }
-
-    public void setEmail_address(String email_address) {
-        this.email_address = email_address;
-    }
-
-    public String getNick_name() {
-        return nick_name;
-    }
-
-    public void setNick_name(String nick_name) {
-        this.nick_name = nick_name;
-    }
-
-    public LocalDateTime getReg_dt() {
-        return reg_dt;
-    }
-
-    public void setReg_dt(LocalDateTime reg_dt) {
-        this.reg_dt = reg_dt;
-    }
-
-    public String getUserRole() {
-        return user_auth;
-    }
-
-    public void setUserRole(String userRole) {
-        this.user_auth = userRole;
-    }
-
-    public String getPictureURL() {
-        return pictureURL;
-    }
-
-    public void setPictureURL(String pictureURL) {
-        this.pictureURL = pictureURL;
-    }
-
-    @Override
-    public String toString() {
-        return "Member{" +
-                "member_id=" + member_id +
-                ", email_address='" + email_address + '\'' +
-                ", nick_name='" + nick_name + '\'' +
-                ", reg_dt=" + reg_dt + '\'' +
-                '}';
     }
 }
--- src/main/java/bside/writing/controller/MemberTestController.java
+++ src/main/java/bside/writing/controller/MemberTestController.java
@@ -1,25 +1,8 @@
 package bside.writing.controller;
 
-import bside.writing.Member.Member;
 import bside.writing.Repository.MemberRepository;
 import bside.writing.Service.MemberService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-
-import javax.servlet.http.HttpServletResponse;
-import java.util.List;
-import bside.writing.Member.Member;
-import bside.writing.Repository.MemberRepository;
-import bside.writing.Service.MemberService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-
-import javax.servlet.http.HttpServletResponse;
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Optional;
 
 @RestController
 public class MemberTestController {
@@ -30,32 +13,4 @@ public class MemberTestController {
         this.memberService = memberService;
         this.memberRepository = memberRepository;
     }
-/*
-    @RequestMapping(value = "/members", method = RequestMethod.GET)
-    @ResponseStatus(value = HttpStatus.OK)
-    public boolean findMember(@RequestParam String email_address){
-        System.out.println(email_address);
-        Optional<List<Member>> findResult = memberRepository.findByUserEmail(email_address);
-        if(findResult.isPresent()) return true;
-        else return false;
-    }
-*/
-    //필요없음 legacy
-
-    /*@PostMapping(value = "/members" )
-    public String create(@RequestBody Member member, HttpServletResponse response) throws Exception{
-        LocalDateTime curr = LocalDateTime.now();
-        member.setReg_dt(curr);
-        memberService.join(member);
-        return member.toString();
-        *//*if(!memberService.validateDuplicateMember(member)){
-            response.setStatus(406);
-            return "Duplicate User";
-        }
-        else{
-            memberService.join(member);
-            return member.toString();
-        }*//*
-    }*/
-    //필요없음 legacy
 }
