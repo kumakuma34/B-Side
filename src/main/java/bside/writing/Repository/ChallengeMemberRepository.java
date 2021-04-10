600b3e514a6124ec4aec93899387f5fc13f14008
kumakuma34 <qgqg264@gmail.com>
2021-04-11 00:00:09 +0900

    feat: Challenge join duplicate check logic
    challeng_member duplicate check logic add
    
    Signed-off-by: kumakuma34 <qgqg264@gmail.com>

--- src/main/java/bside/writing/Repository/ChallengeMemberRepository.java
+++ src/main/java/bside/writing/Repository/ChallengeMemberRepository.java
@@ -9,4 +9,7 @@ import java.util.List;
 public interface ChallengeMemberRepository extends JpaRepository<ChallengeMember,Long> {
     @Query("select c.memberId from ChallengeMember c WHERE c.challengeId = ?1")
     List<Long> findAllByChallengeId(Long challenge_id);
+
+    @Query("select c.challengeMemberId from ChallengeMember c WHERE c.challengeId = ?1 AND c.memberId = ?2")
+    List<Long> findDuplicate(Long challenge_id , Long member_id);
 }
