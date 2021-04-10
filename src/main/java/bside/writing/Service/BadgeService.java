156456761043a130a3244f4b3153b0024e72c805
developingbear <developingbear@gmail.com>
2021-04-10 17:29:26 +0900

    refactor: GET Badge
    badge 조회 api 수정
    - badge code별 조회 삭제
    - badge url 추가
    - badge achieve 추가
    - badge default img 추가
    - badge achieve img 추가

--- src/main/java/bside/writing/Service/BadgeService.java
+++ src/main/java/bside/writing/Service/BadgeService.java
@@ -5,62 +5,96 @@ import bside.writing.domain.badge.Badge;
 import bside.writing.enums.BadgeCode;
 import bside.writing.dto.BadgeDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
 public class BadgeService {
+
+    @Value("${SERVER_URL}")
+    private String SERVER_URL;
     private final BadgeRepository badgeRepository;
+
+    public Map<String, List> getDefaultResponse(){
+        Map<String, List> defaultResponse = new LinkedHashMap<>();
+        Arrays.stream(BadgeCode.values()).forEach(
+                (code)->{
+                    List list = new ArrayList();
+                    for (Object badgeValue : code.getCriteria()){
+                        Map<String, Object> cur = new LinkedHashMap<>();
+                        cur.put("badge_value", badgeValue);
+                        cur.put("achieve", false);
+                        cur.put("image_url", SERVER_URL + code.name() + badgeValue + "-default.png");
+                        list.add(cur);
+                    }
+                    defaultResponse.put(code.name(), list);
+                }
+        );
+        return defaultResponse;
+    }
 
     @Transactional
     public List<Badge> getBadgesByMemberId(Long memberId){
+        Map<String, List> defaultResponse = getDefaultResponse();
         return badgeRepository.findByMemberId(memberId)
                 .orElseThrow(()->new NoSuchElementException("user does not have any badge"));
     }
 
     @Transactional
     public List<Badge> getBadgeByMemberIdAndBadgeCode(Long memberId, String badgeCode){
+        if(!Arrays.stream(BadgeCode.values()).anyMatch(curBadgeCode -> curBadgeCode.name().equals(badgeCode)))
+            throw new IllegalArgumentException("no such badgecode");
+
         return badgeRepository.findByMemberIdAndBadgeCode(memberId, badgeCode)
-                .orElseThrow(() -> {
-                    if (!Arrays.stream(BadgeCode.values()).anyMatch(curBadgeCode -> curBadgeCode.name().equals(badgeCode)))
-                        return new IllegalArgumentException("no such badgecode");
-                    else
-                        return new NoSuchElementException("user does not have any badge");
-                    }
-                );
+                .orElseThrow(() -> new NoSuchElementException("user does not have any badge"));
     }
 
     public List<BadgeDto> toDtoList(List<Badge> badgeList){
-
         return  badgeList.stream().map((entity) -> BadgeDto.builder()
                 .badgeCode(entity.getBadgeCode())
                 .badgeValue(entity.getBadgeValue())
+                .badgeUrl(SERVER_URL + entity.getBadgeCode() + entity.getBadgeValue() + ".png")
                 .build())
                 .collect(Collectors.toList());
     }
 
-    public Map<String, List> toMapByBadgeCode(List<BadgeDto> badgeDtoList){
+    public Map<String, List> listToMap(List<BadgeDto> badgeDtoList){
         Collections.sort(badgeDtoList);
 
-        Map<String, List> badgeDtoMap = new HashMap<>();
-
+        Map<String, List> badgeDtoMap = new LinkedHashMap<>();
         for(BadgeDto badgeDto : badgeDtoList){
             String badgeCodeStr = badgeDto.getBadgeCode();
 
             if(!badgeDtoMap.containsKey(badgeCodeStr))
                 badgeDtoMap.put(badgeCodeStr, new ArrayList());
-
-            List list = badgeDtoMap.get(badgeDto.getBadgeCode());
-            list.add(badgeDto.getBadgeValue());
-
+            List curCodeBadgelist = badgeDtoMap.get(badgeDto.getBadgeCode());
+            List curBadge = Arrays.asList(badgeDto.getBadgeValue(), badgeDto.getBadgeUrl());
+            curCodeBadgelist.add(curBadge);
         }
 
-
         return badgeDtoMap;
+    }
+
+    public Map<String, List> updateList(List<BadgeDto> badgeDtoList){
+        Map<String, List> updatedResponse = getDefaultResponse();
+        Collections.sort(badgeDtoList);
+
+        for(int i = 0; i < badgeDtoList.size(); ++i){
+            BadgeDto curBadgeDto = badgeDtoList.get(i);
+            List curBadgeList = updatedResponse.get(curBadgeDto.getBadgeCode());
+
+            int index = BadgeCode.valueOf(curBadgeDto.getBadgeCode()).getCriteria().indexOf(curBadgeDto.getBadgeValue());
+            Map<String, Object> map = (Map<String, Object>) curBadgeList.get(index);
+            map.replace("achieve", true);
+            map.replace("image_url", SERVER_URL + curBadgeDto.getBadgeCode() + curBadgeDto.getBadgeValue() + ".png");
+        }
+        return updatedResponse;
     }
 
 }
--- src/main/java/bside/writing/enums/BadgeCode.java
+++ src/main/java/bside/writing/enums/BadgeCode.java
@@ -1,9 +1,22 @@
 package bside.writing.enums;
 
+import java.util.Arrays;
+import java.util.List;
+
 public enum BadgeCode {
-    ARTICLE_COMMIT,
-    CHALLENGE_PARTICIPATE,
-    CHALLENGE_FINISH,
-    LIKE_COUNT
+    ARTICLE_COMMIT(Arrays.asList("1","10","50"))
+    ,CHALLENGE_PARTICIPATE(Arrays.asList("1","6","12"))
+    ,CHALLENGE_FINISH(Arrays.asList("1","2","8"))
+    ,LIKE_COUNT(Arrays.asList("1","10","20"))
     ;
+
+    private List criteria;
+
+    BadgeCode(List criteria) {
+        this.criteria = criteria;
+    }
+
+    public List getCriteria() {
+        return criteria;
+    }
 }
