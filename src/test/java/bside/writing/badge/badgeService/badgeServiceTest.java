8e2d76d20723b6d738794a966c05d75e1e90775b
developingbear <developingbear@gmail.com>
2021-04-20 20:40:26 +0900

    fix: badge

--- src/test/java/bside/writing/badge/badgeService/badgeServiceTest.java
+++ src/test/java/bside/writing/badge/badgeService/badgeServiceTest.java
@@ -1,15 +1,26 @@
 package bside.writing.badge.badgeService;
 
 import bside.writing.Service.BadgeService;
+import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Map;
 
 @SpringBootTest
 @Transactional
 public class badgeServiceTest {
     @Autowired
     BadgeService badgeService;
+
+    @Test
+    public void search_badge(){
+        Long memberId = 30L;
+        Map<String, List> stringListMap = badgeService.makeResponseList(memberId);
+        System.out.println("stringListMap = " + stringListMap);
+
+    }
 
 }
